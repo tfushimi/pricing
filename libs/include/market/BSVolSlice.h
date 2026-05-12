dc7de352c6cc67d30be568112bf272d70dfd5cba
Takahiro Fushimi <taka.6765@gmail.com>
2026-05-12 16:14:52 -0400

    refactor BSVolSlice

--- libs/include/market/BSVolSlice.h
+++ libs/include/market/BSVolSlice.h
@@ -12,14 +12,22 @@ namespace market {
 // as a function of strike, used by BS pricers and digital call pricing via dVol/dStrike.
 class BSVolSlice {
    public:
-    BSVolSlice() = default;
+    BSVolSlice(const double forward, const double time) : _forward(forward), _time(time) {
+        if (_forward <= 0.0) {
+            throw std::invalid_argument("BSVolSlice: forward must be positive");
+        }
+        if (_time <= 0.0) {
+            throw std::invalid_argument("BSVolSlice: time must be positive");
+        }
+    }
+
     virtual ~BSVolSlice() = default;
 
     // Returns the forward price of the underlying at this expiry.
-    virtual double forward() const = 0;
+    double forward() const { return _forward; }
 
     // Returns the time to expiry in years.
-    virtual double time() const = 0;
+    double time() const { return _time; }
 
     // Returns the Black-Scholes implied volatility at the given strike.
     virtual double vol(double strike) const = 0;
@@ -29,6 +37,10 @@ class BSVolSlice {
         constexpr double dK = 1e-4;
         return (vol(strike + dK) - vol(strike - dK)) / (2.0 * dK);
     }
+
+   private:
+    double _forward;
+    double _time;
 };
 
 // Represents a point on a volatility surface
@@ -41,16 +53,12 @@ struct VolPoint {
 class FlatVolSlice final : public BSVolSlice {
    public:
     FlatVolSlice(const double forward, const double time, const double vol)
-        : _forward(forward), _time(time), _vol(vol) {}
+        : BSVolSlice(forward, time), _vol(vol) {}
 
-    double forward() const override { return _forward; }
-    double time() const override { return _time; }
     double vol(const double) const override { return _vol; }
     double dVolDStrike(const double) const override { return 0.0; }
 
    private:
-    double _forward;
-    double _time;
     double _vol;
 };
 
@@ -58,23 +66,13 @@ class FlatVolSlice final : public BSVolSlice {
 class SVIVolSlice final : public BSVolSlice {
    public:
     SVIVolSlice(const double forward, const double time, SVIParams params)
-        : _forward(forward), _time(time), _svi(params) {
-        if (_forward <= 0.0) {
-            throw std::invalid_argument("SVIVolSlice: forward must be positive");
-        }
-        if (_time <= 0.0) {
-            throw std::invalid_argument("SVIVolSlice: time must be positive");
-        }
-    }
-
-    double forward() const override { return _forward; }
-    double time() const override { return _time; }
+        : BSVolSlice(forward, time), _svi(params) {}
 
     double vol(const double strike) const override {
         if (strike <= 0.0) {
             throw std::invalid_argument("SVIVolSlice: strike must be positive");
         }
-        return _svi.impliedVolAtStrike(_forward, strike, _time);
+        return _svi.impliedVolAtStrike(forward(), strike, time());
     }
 
     /**
@@ -87,16 +85,14 @@ class SVIVolSlice final : public BSVolSlice {
         if (strike <= 0.0) {
             throw std::invalid_argument("SVIVolSlice: strike must be positive");
         }
-        const double k = std::log(_forward / strike);
+        const double k = std::log(forward() / strike);
         const double w = _svi.totalVariance(k);
         const double dw = _svi.dTotalVarianceDk(k);
-        const double sigma = std::sqrt(w / _time);
-        return -dw / (2.0 * sigma * _time * strike);
+        const double sigma = std::sqrt(w / time());
+        return -dw / (2.0 * sigma * time() * strike);
     }
 
    private:
-    double _forward;
-    double _time;
     vol::SVI _svi;
 };
 }  // namespace market
